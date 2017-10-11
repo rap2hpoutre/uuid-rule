4fddc58aab33a26ef4401979f358ac4adc82eb10
rap2h <raphaelht@gmail.com>
2017-10-11 09:56:04 +0200

    init

---
+++ src/UuidRule.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Rap2hpoutre\UuidRule;
+
+use Illuminate\Contracts\Validation\Rule;
+
+class Uuid implements Rule
+{
+
+    /**
+     * Determine if the validation rule passes.
+     *
+     * @param  string  $attribute
+     * @param  mixed  $value
+     * @return bool
+     */
+    public function passes($attribute, $value)
+    {
+        return is_string($value) && preg_match('/^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/', $value) === 1;
+    }
+
+    /**
+     * Get the validation error message.
+     *
+     * @return string
+     */
+    public function message()
+    {
+        return trans('validation.uuid-rule') ?:  'The :attribute must be a UUID.';
+    }
+}
